6fec995391a61bf4db49e1562961698d09e0fcbc
wickeddd3 <pablophilipandrew@gmail.com>
2026-04-17 00:19:40 +0800

    build: add vite config for alias resolver, server, and build output options

--- chat-app-frontend/vite.config.ts
+++ chat-app-frontend/vite.config.ts
@@ -1,8 +1,35 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      // This maps the '@' symbol to 'src' folder
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  server: {
+    host: true, // Listen on all local IPs
+    port: 5173,
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          // Creates a separate chunk for everything in node_modules
+          if (id.includes("node_modules")) {
+            return id
+              .toString()
+              .split("node_modules/")[1]
+              .split("/")[0]
+              .toString();
+          }
+        },
+      },
+    },
+  },
 });
