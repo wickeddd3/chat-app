24385ef99462c56ef432fe3798dd135367949642
wickeddd3 <pablophilipandrew@gmail.com>
2026-04-23 19:29:44 +0800

    feat: add eslint, prettier, and editor config

--- chat-app-backend/src/config/cors-origins.ts
+++ chat-app-backend/src/config/cors-origins.ts
@@ -2,7 +2,4 @@ export const LOCAL_APP = `http://localhost:${process.env.PORT}`;
 
 export const PRODUCTION_APP = `${process.env.APP_URL}`;
 
-export const ALLOWED_ORIGINS =
-  process.env.NODE_ENV === "production"
-    ? [PRODUCTION_APP]
-    : [LOCAL_APP, PRODUCTION_APP];
+export const ALLOWED_ORIGINS = process.env.NODE_ENV === "production" ? [PRODUCTION_APP] : [LOCAL_APP, PRODUCTION_APP];
