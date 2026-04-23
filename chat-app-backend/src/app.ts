d72ce37c38d6c708ec142211410ba50c322a5abe
wickeddd3 <pablophilipandrew@gmail.com>
2026-04-23 17:55:21 +0800

    feat: add app that handles server logic initialization

---
+++ chat-app-backend/src/app.ts
@@ -0,0 +1,55 @@
+import express, { type Application } from "express";
+import { createServer, type Server as HttpServer } from "http";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
+import compression from "compression";
+import { Controller } from "@/interfaces/controller.interface";
+import { ALLOWED_ORIGINS } from "@/config/cors-origins";
+import { toNodeHandler } from "better-auth/node";
+import { auth } from "@/lib/better-auth";
+
+export class App {
+  public express: Application;
+  public port: number;
+  public server: HttpServer;
+
+  constructor(controllers: Controller[], port: number) {
+    this.express = express();
+    this.port = port;
+    this.server = createServer(this.express);
+
+    this.initializeMiddlewares();
+    this.initializeAuth();
+    this.initializeControllers(controllers);
+  }
+
+  public start(): void {
+    this.server.listen(this.port, () => {
+      console.log(`Server running on port ${this.port}`);
+    });
+  }
+
+  private initializeMiddlewares(): void {
+    this.express.use(helmet());
+    this.express.use(
+      cors({
+        origin: ALLOWED_ORIGINS,
+        credentials: true,
+      }),
+    );
+    this.express.use(morgan("dev"));
+    this.express.use(express.json({ limit: "10mb" }));
+    this.express.use(compression());
+  }
+
+  private initializeAuth(): void {
+    this.express.all("/api/auth/*splat", toNodeHandler(auth)); // Better-auth route for authentication
+  }
+
+  private initializeControllers(controllers: Controller[]): void {
+    controllers.forEach((controller: Controller) => {
+      this.express.use("/api", controller.router);
+    });
+  }
+}
--- chat-app-backend/src/index.ts
+++ chat-app-backend/src/index.ts
@@ -1,43 +1,9 @@
-import express from "express";
-import http from "http";
-import cors from "cors";
-import dotenv from "dotenv";
-import { Server } from "socket.io";
+import { PORT } from "@/config/app.config";
+import { validateEnv } from "@/lib/validate-env";
+import { App } from "@/app";
 
-dotenv.config();
+validateEnv();
 
-const app = express();
-app.use(cors());
+const app = new App([], PORT);
 
-const port = process.env.PORT || 4000;
-const server = http.createServer(app);
-const io = new Server(server, {
-  cors: {
-    origin: process.env.APP_URL, // Your React dev URL
-    methods: ["GET", "POST"],
-  },
-});
-
-io.on("connection", (socket) => {
-  console.log(`User Connected: ${socket.id}`);
-
-  // Joining a specific chat room
-  socket.on("join_room", (data) => {
-    socket.join(data);
-    console.log(`User ${socket.id} joined room: ${data}`);
-  });
-
-  // Handling message sent from client
-  socket.on("send_message", (data) => {
-    // Broadcast message to everyone in the room (including sender)
-    io.to(data.room).emit("receive_message", data);
-  });
-
-  socket.on("disconnect", () => {
-    console.log("User Disconnected", socket.id);
-  });
-});
-
-server.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
+app.start();
