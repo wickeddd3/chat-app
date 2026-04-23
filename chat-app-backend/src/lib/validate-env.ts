7970c577fccdd3db1a3cca2494daa9ae8623fb1f
wickeddd3 <pablophilipandrew@gmail.com>
2026-04-23 17:54:30 +0800

    feat: add validate-env lib

---
+++ chat-app-backend/src/lib/validate-env.ts
@@ -0,0 +1,13 @@
+import { cleanEnv, str, port } from "envalid";
+
+export const validateEnv = (): void => {
+  cleanEnv(process.env, {
+    NODE_ENV: str({
+      choices: ["development", "production"],
+    }),
+    PORT: port({ default: 4000 }),
+    DATABASE_URL: str(),
+    APP_URL: str(),
+    BETTER_AUTH_SECRET: str(),
+  });
+};
