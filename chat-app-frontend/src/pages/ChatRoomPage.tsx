f59ae25a7d8b4fd7f7c817154b23fc2858d7fdcd
wickeddd3 <pablophilipandrew@gmail.com>
2026-04-25 04:08:42 +0800

    cut: remove element wrapper

--- chat-app-frontend/src/pages/ChatRoomPage.tsx
+++ chat-app-frontend/src/pages/ChatRoomPage.tsx
@@ -1,9 +1,5 @@
 import { ChatRoom } from "@/features/message/chat-room";
 
 export default function ChatRoomPage() {
-  return (
-    <div className="flex-1">
-      <ChatRoom />
-    </div>
-  );
+  return <ChatRoom />;
 }
