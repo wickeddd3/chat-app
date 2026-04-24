bc37b1762b20711321b5d46e00492e51b267b829
wickeddd3 <pablophilipandrew@gmail.com>
2026-04-24 22:34:55 +0800

    feat: set max width to md

--- chat-app-frontend/src/pages/SignInPage.tsx
+++ chat-app-frontend/src/pages/SignInPage.tsx
@@ -3,7 +3,7 @@ import { SignInForm } from "@/features/auth/sign-in";
 
 export default function SignInPage() {
   return (
-    <div className="flex w-full max-w-sm flex-col gap-6">
+    <div className="flex w-full max-w-md flex-col gap-6">
       <a href="#" className="flex items-center gap-2 self-center font-medium">
         <div className="flex size-6 items-center justify-center rounded-md bg-primary text-primary-foreground">
           <GalleryVerticalEnd className="size-4" />
