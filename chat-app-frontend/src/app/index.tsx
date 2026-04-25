2c1dbd1c328d21c3593a6f88a86915ab3fa7828c
wickeddd3 <pablophilipandrew@gmail.com>
2026-04-26 01:43:29 +0800

    feat: wrap RouterProvider in QueryClientProvider

--- chat-app-frontend/src/app/index.tsx
+++ chat-app-frontend/src/app/index.tsx
@@ -1,6 +1,12 @@
 import { RouterProvider } from "react-router";
 import { router } from "@/app/router";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "@/shared/lib/query.client";
 
 export function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  );
 }
