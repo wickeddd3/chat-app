55b0332778f7bae5d34c2fe2f70228ad14e4f676
wickeddd3 <pablophilipandrew@gmail.com>
2026-04-27 00:22:44 +0800

    feat: use updated api

--- chat-app-frontend/src/features/message/chat-room/api/messages.api.ts
+++ chat-app-frontend/src/features/message/chat-room/api/messages.api.ts
@@ -3,7 +3,9 @@ import apiRequest from "@/shared/lib/axios.client";
 
 export async function getMessages(roomId: string): Promise<Partial<Message[]>> {
   try {
-    const { data } = await apiRequest({ url: `/api/messages/${roomId}` }).get();
+    const { data } = await apiRequest({
+      url: `/api/messages/inbox/${roomId}`,
+    }).get();
     return data;
   } catch (error) {
     console.error("Error fetching messages:", error);
