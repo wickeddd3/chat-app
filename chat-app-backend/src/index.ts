2bf481d46ca53039d0777903470ac8e9c0d4b288
wickeddd3 <pablophilipandrew@gmail.com>
2026-04-26 16:18:56 +0800

    feat: register MessagesController on app entry point

--- chat-app-backend/src/index.ts
+++ chat-app-backend/src/index.ts
@@ -2,9 +2,10 @@ import { PORT } from "@/config/app.config";
 import { validateEnv } from "@/lib/validate-env";
 import { App } from "@/app";
 import { UsersController } from "@/modules/user/users.controller";
+import { MessagesController } from "./modules/message/messages.controller";
 
 validateEnv();
 
-const app = new App([new UsersController()], PORT);
+const app = new App([new UsersController(), new MessagesController()], PORT);
 
 app.start();
