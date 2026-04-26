2faf969062335390bd3cd826b9df7e4126c3eec8
wickeddd3 <pablophilipandrew@gmail.com>
2026-04-26 16:23:16 +0800

    feat: add Message type

--- chat-app-frontend/src/entities/message/index.ts
+++ chat-app-frontend/src/entities/message/index.ts
@@ -1,2 +1,3 @@
 export { AvatarWithBadge } from "./ui/AvatarWithBadge";
 export { AuthorAvatar } from "./ui/AuthorAvatar";
+export type { Message } from "./model/message.types";
---
+++ chat-app-frontend/src/entities/message/model/message.types.ts
@@ -0,0 +1,7 @@
+export interface Message {
+  id: string;
+  content: string;
+  roomId: string;
+  authorId: string;
+  createdAt: string;
+}
