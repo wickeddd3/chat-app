44edc879d5547f655b03eba9c4d53b4180fe6be4
wickeddd3 <pablophilipandrew@gmail.com>
2026-04-26 16:31:11 +0800

    feat: handle message sending optimistic ui update

--- chat-app-frontend/src/features/message/chat-room/ui/MessageInput.tsx
+++ chat-app-frontend/src/features/message/chat-room/ui/MessageInput.tsx
@@ -2,27 +2,63 @@ import { Send } from "lucide-react";
 import { webSocketClient } from "@/shared/lib/socket-io.client";
 import { useState } from "react";
 
-export function MessageInput({ room }: { room: string }) {
+export function MessageInput({
+  roomId,
+  author,
+  onMessageSent,
+}: {
+  roomId: string;
+  author?: Partial<{
+    id: string;
+    name: string;
+    image: string | null;
+  }>;
+  onMessageSent: (message: any) => void;
+}) {
   const [message, setMessage] = useState("");
 
   const sendMessage = (e: React.SubmitEvent<HTMLFormElement>) => {
     e.preventDefault();
-    if (message !== "") {
-      const messageData = {
-        room,
-        text: message,
-        time: new Date().toLocaleTimeString([], {
-          hour: "2-digit",
-          minute: "2-digit",
-        }),
-      };
-      webSocketClient.emit("send_message", messageData);
-      setMessage("");
-    }
+
+    if (message.trim() === "") return;
+
+    // 1. Generate a temporary unique ID
+    const clientId = window.crypto.randomUUID();
+
+    // 2. Create the message object with optimistic data
+    const messageData = {
+      clientId,
+      roomId,
+      content: message,
+      author: {
+        id: author?.id,
+        name: author?.name,
+        image: author?.image,
+      },
+      time: new Date().toLocaleTimeString([], {
+        hour: "2-digit",
+        minute: "2-digit",
+      }),
+      isSending: true,
+    };
+
+    // 3. Update UI immediately
+    onMessageSent(messageData);
+
+    // 4. Send to server
+    webSocketClient.emit("send_message", {
+      roomId,
+      content: message,
+      clientId,
+    });
+    setMessage("");
   };
 
   return (
-    <form onSubmit={sendMessage} className="w-full flex gap-2">
+    <form
+      onSubmit={sendMessage}
+      className="w-full flex gap-2 bg-gray-100 rounded-lg"
+    >
       <input
         className="flex-1 border p-4 rounded border-none outline-0"
         value={message}
