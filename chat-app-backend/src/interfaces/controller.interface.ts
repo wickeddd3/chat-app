5604a8aeb6d824381859b366a28c464e876ade9c
wickeddd3 <pablophilipandrew@gmail.com>
2026-04-27 14:53:34 +0800

    feat: add ControllerRequest interface

--- chat-app-backend/src/interfaces/controller.interface.ts
+++ chat-app-backend/src/interfaces/controller.interface.ts
@@ -1,6 +1,11 @@
-import { Router } from "express";
+import type { Request, Router } from "express";
+import type { User } from "@/prisma/client";
 
 export interface Controller {
   path: string;
   router: Router;
+}
+
+export interface ControllerRequest extends Request {
+  user?: User;
 }
