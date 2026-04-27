8e8ff75363245fb142ee572d2b2fbd3e99f9a728
wickeddd3 <pablophilipandrew@gmail.com>
2026-04-28 02:59:46 +0800

    feat: update import path

--- chat-app-frontend/src/main.tsx
+++ chat-app-frontend/src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./app/styles/index.css";
-import { App } from "./app/index.tsx";
+import "@/app/styles/index.css";
+import { App } from "@/app/index.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
