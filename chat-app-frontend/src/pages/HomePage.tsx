260a650b822d40c83ad1ca2b40387e4c943e0ff5
wickeddd3 <pablophilipandrew@gmail.com>
2026-04-28 02:27:18 +0800

    feat: add HomePage temporary content

--- chat-app-frontend/src/pages/HomePage.tsx
+++ chat-app-frontend/src/pages/HomePage.tsx
@@ -1,3 +1,8 @@
 export default function HomePage() {
-  return <h1>Home Page</h1>;
+  return (
+    <div className="w-full h-full flex flex-col justify-center items-center">
+      <h1 className="text-3xl font-bold text-gray-400">Home Page</h1>
+      <h6 className="text-lg font-normal text-gray-400">Coming Soon</h6>
+    </div>
+  );
 }
