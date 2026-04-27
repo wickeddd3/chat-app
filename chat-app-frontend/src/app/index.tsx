a4be44c05a117a5f5e7cb58dad1b0c661b183bde
wickeddd3 <pablophilipandrew@gmail.com>
2026-04-28 03:00:16 +0800

    feat: wrap router in TooltipProvider

--- chat-app-frontend/src/app/index.tsx
+++ chat-app-frontend/src/app/index.tsx
@@ -2,11 +2,14 @@ import { RouterProvider } from "react-router";
 import { router } from "@/app/router";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "@/shared/lib/query.client";
+import { TooltipProvider } from "@/shared/ui/shadcn/tooltip";
 
 export function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
+      <TooltipProvider>
+        <RouterProvider router={router} />
+      </TooltipProvider>
     </QueryClientProvider>
   );
 }
