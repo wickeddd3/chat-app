8912f14ee23dc1359716325ad1cdd3774300f17c
wickeddd3 <pablophilipandrew@gmail.com>
2026-04-27 14:38:35 +0800

    feat: add username with better-auth

--- chat-app-backend/src/lib/better-auth.ts
+++ chat-app-backend/src/lib/better-auth.ts
@@ -3,13 +3,25 @@ import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { prisma } from "@/lib/prisma";
 import { APP_URL } from "@/config/app.config";
+import { username } from "better-auth/plugins";
 
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
     provider: "postgresql",
   }),
   trustedOrigins: [`${APP_URL}`],
+  baseURL: APP_URL,
   emailAndPassword: {
     enabled: true,
   },
+  plugins: [
+    username({
+      minUsernameLength: 6,
+      maxUsernameLength: 100,
+      displayUsernameValidator: (displayUsername) => {
+        // Allow only alphanumeric characters, underscores, and hyphens
+        return /^[a-zA-Z0-9_-]+$/.test(displayUsername);
+      },
+    }),
+  ],
 });
