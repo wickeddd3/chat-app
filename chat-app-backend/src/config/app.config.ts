7a5643bb487f7653fd1c3d6bcefe3584bd3ea267
wickeddd3 <pablophilipandrew@gmail.com>
2026-05-01 02:02:14 +0800

    feat: add REDIS_URL config

--- chat-app-backend/src/config/app.config.ts
+++ chat-app-backend/src/config/app.config.ts
@@ -3,3 +3,5 @@ export const APP_URL = process.env.APP_URL;
 export const PORT = process.env.PORT ? parseInt(process.env.PORT, 10) : 4000;
 
 export const DATABASE_URL = process.env.DATABASE_URL;
+
+export const REDIS_URL = process.env.REDIS_URL;
