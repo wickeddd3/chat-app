7c71ed01776f59f07e9d4118061d2530537f981f
wickeddd3 <pablophilipandrew@gmail.com>
2026-05-02 16:24:09 +0800

    feat: check user presence if online

--- chat-app-frontend/src/features/message/chat-inbox/ui/ChatInboxItem.tsx
+++ chat-app-frontend/src/features/message/chat-inbox/ui/ChatInboxItem.tsx
@@ -2,22 +2,27 @@ import { AvatarWithBadge, type InboxItem } from "@/entities/message";
 import { Link } from "react-router";
 import { CheckCheck } from "lucide-react";
 import { dateToString } from "@/shared/utils/date-format";
+import { usePresence } from "@/app/store/PresenceContext";
 
 export function ChatInboxItem({
   inboxItem: {
     lastMessage,
     updatedAt,
-    otherUser: { name, username, image },
+    otherUser: { id, name, username, image },
   },
 }: {
   inboxItem: InboxItem;
 }) {
+  const { isOnline } = usePresence();
+  const targetUserId = id;
+  const online = isOnline(targetUserId);
+
   return (
     <Link
       to={`/messages/@${username}`}
       className="flex items-center gap-4 border-b p-4 text-sm leading-tight whitespace-nowrap last:border-b-0 hover:bg-sidebar-accent hover:text-sidebar-accent-foreground"
     >
-      <AvatarWithBadge imageSrc={image} />
+      <AvatarWithBadge imageSrc={image} isOnline={online} />
       <div className="flex-1 flex flex-col items-start gap-2">
         <div className="flex w-full items-center gap-2">
           <span>{name}</span>
