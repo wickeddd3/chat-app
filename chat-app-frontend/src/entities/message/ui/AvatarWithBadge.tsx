42d62695ce6b7414fee70640bec2dc6e5bad382e
wickeddd3 <pablophilipandrew@gmail.com>
2026-05-02 14:21:27 +0800

    feat: change badge color if isOnline prop exist

--- chat-app-frontend/src/entities/message/ui/AvatarWithBadge.tsx
+++ chat-app-frontend/src/entities/message/ui/AvatarWithBadge.tsx
@@ -1,11 +1,14 @@
+import { cn } from "@/shared/lib/utils";
 import { Avatar, AvatarBadge, AvatarImage } from "@/shared/ui/shadcn/avatar";
 
 export function AvatarWithBadge({
   imageSrc,
   size = "lg",
+  isOnline = false,
 }: {
   imageSrc: string;
   size?: "sm" | "default" | "lg" | undefined;
+  isOnline?: boolean;
 }) {
   return (
     <Avatar size={size}>
@@ -13,7 +16,12 @@ export function AvatarWithBadge({
         src={imageSrc || "/default-avatar.jpg"}
         alt="avatar-with-badge"
       />
-      <AvatarBadge className="bg-green-600 dark:bg-green-800" />
+      <AvatarBadge
+        className={cn(
+          "bg-gray-400 dark:bg-gray-300",
+          `${isOnline && "bg-green-600 dark:bg-green-800"}`,
+        )}
+      />
     </Avatar>
   );
 }
