ce99d4fea24768e9b17e1318cf73413fabb087d9
wickeddd3 <pablophilipandrew@gmail.com>
2026-05-02 16:19:16 +0800

    feat: initialize websocket connection and heartbeat listener on chat-layout

--- chat-app-frontend/src/app/layouts/chat-layout/index.tsx
+++ chat-app-frontend/src/app/layouts/chat-layout/index.tsx
@@ -1,8 +1,15 @@
+import { useAuth } from "@/entities/auth";
 import { Toaster } from "@/shared/ui/shadcn/sonner";
 import { ChatSidebar } from "@/widgets/navigation";
 import { Outlet } from "react-router";
+import { useWebSocketConnect } from "@/features/message/websocket-connection";
+import { useHeartbeat } from "@/features/message/online-presence";
 
 export function ChatLayout() {
+  const { authSession } = useAuth();
+  useWebSocketConnect(!!authSession);
+  useHeartbeat(!!authSession);
+
   return (
     <div className="flex h-screen w-full overflow-hidden bg-background">
       <aside className="flex w-20 flex-col border-r bg-muted/30">
