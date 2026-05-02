65269868acaba8f58c74cc4ce05485d8ee58bf1b
wickeddd3 <pablophilipandrew@gmail.com>
2026-05-02 16:26:10 +0800

    feat: check user presence if online

--- chat-app-frontend/src/features/message/chat-room/ui/MessageHeader.tsx
+++ chat-app-frontend/src/features/message/chat-room/ui/MessageHeader.tsx
@@ -1,3 +1,4 @@
+import { usePresence } from "@/app/store/PresenceContext";
 import { AvatarWithBadge } from "@/entities/message";
 import type { User } from "@/entities/user";
 
@@ -6,10 +7,14 @@ export function MessageHeader({
 }: {
   user: Partial<User | null | undefined>;
 }) {
+  const { isOnline } = usePresence();
+  const targetUserId = user?.id || "";
+  const online = isOnline(targetUserId);
+
   return (
     <div className="w-full py-5 px-4">
       <div className="flex items-center gap-4">
-        <AvatarWithBadge imageSrc={user?.image || ""} />
+        <AvatarWithBadge imageSrc={user?.image || ""} isOnline={online} />
         <h1 className="text-md font-medium">{user?.name}</h1>
       </div>
     </div>
