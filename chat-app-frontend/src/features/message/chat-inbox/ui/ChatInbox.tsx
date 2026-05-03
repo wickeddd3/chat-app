06167da52e6ef6c7d01eb7ebbd9c752b7fcd619c
wickeddd3 <pablophilipandrew@gmail.com>
2026-05-03 14:08:30 +0800

    feat: add inbox tabs and filter online users

--- chat-app-frontend/src/features/message/chat-inbox/ui/ChatInbox.tsx
+++ chat-app-frontend/src/features/message/chat-inbox/ui/ChatInbox.tsx
@@ -1,24 +1,91 @@
 import { useInbox } from "../model/useInbox";
-import { ChatInboxItem } from "./ChatInboxItem";
-import { LoadingPlaceholder } from "./LoadingPlaceholder";
-import { EmptyPlaceholder } from "./EmptyPlaceholder";
+import {
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger,
+} from "@/shared/ui/shadcn/tabs";
+import { SearchField } from "@/shared/ui/SearchField";
+import { ChatInboxResults } from "./ChatInboxResults";
+import { useMemo, useState } from "react";
+import { usePresence } from "@/app/store/PresenceContext";
 
 export function ChatInbox() {
   const { inbox, isLoading, isEmpty } = useInbox();
+  const { onlineUsers, isOnline } = usePresence();
+
+  const [query, setQuery] = useState("");
+
+  const allInbox = useMemo(() => {
+    return inbox.map((item) => ({
+      ...item,
+      online: isOnline(item.otherUser?.id),
+    }));
+  }, [inbox, onlineUsers]);
+
+  const filteredByOnline = useMemo(() => {
+    return allInbox.filter((item) => item.online);
+  }, [allInbox]);
 
   return (
     <div className="flex-1 flex flex-col border-r">
-      <div className="border-b p-4">
+      <div className="p-4">
         <h1 className="text-base font-medium text-foreground">Chat Inbox</h1>
       </div>
-      <div className="flex-1 w-full overflow-y-auto">
-        {isLoading && <LoadingPlaceholder />}
+      <div className="flex-1 w-full h-full">
+        <SearchField
+          value={query}
+          onChange={setQuery}
+          className="px-4 pb-6 pt-1"
+        />
 
-        {inbox.map((inboxItem) => (
-          <ChatInboxItem key={inboxItem.roomId} inboxItem={inboxItem} />
-        ))}
-
-        {isEmpty && <EmptyPlaceholder />}
+        <Tabs defaultValue="all" className="w-full h-full">
+          <TabsList className="w-fit px-4 bg-transparent">
+            <TabsTrigger
+              value="all"
+              className="px-8 cursor-pointer rounded-full"
+            >
+              All
+            </TabsTrigger>
+            <TabsTrigger value="online" className="px-8 cursor-pointer">
+              Online
+            </TabsTrigger>
+            <TabsTrigger value="unread" className="px-8 cursor-pointer">
+              Unread
+            </TabsTrigger>
+            <TabsTrigger value="groups" className="px-8 cursor-pointer">
+              Groups
+            </TabsTrigger>
+          </TabsList>
+          <TabsContent value="all">
+            <ChatInboxResults
+              results={allInbox}
+              isLoading={isLoading}
+              isEmpty={isEmpty}
+            />
+          </TabsContent>
+          <TabsContent value="online">
+            <ChatInboxResults
+              results={filteredByOnline}
+              isLoading={isLoading}
+              isEmpty={!!!filteredByOnline.length}
+            />
+          </TabsContent>
+          <TabsContent value="unread">
+            <ChatInboxResults
+              results={[]}
+              isLoading={isLoading}
+              isEmpty={true}
+            />
+          </TabsContent>
+          <TabsContent value="groups">
+            <ChatInboxResults
+              results={[]}
+              isLoading={isLoading}
+              isEmpty={true}
+            />
+          </TabsContent>
+        </Tabs>
       </div>
     </div>
   );
