4daef9841cb40075816f7080d9000f5244e33f02
wickeddd3 <pablophilipandrew@gmail.com>
2026-05-03 14:07:38 +0800

    feat: add search field

--- chat-app-frontend/src/features/user/user-list/ui/UserList.tsx
+++ chat-app-frontend/src/features/user/user-list/ui/UserList.tsx
@@ -1,18 +1,26 @@
+import { SearchField } from "@/shared/ui/SearchField";
 import { useUsers } from "../model/useUsers";
 import { EmptyPlaceholder } from "./EmptyPlaceholder";
 import { LoadingPlaceholder } from "./LoadingPlaceholder";
 import { UserListItem } from "./UserListItem";
+import { useState } from "react";
 
 export function UserList() {
   const { users, isLoading, isEmpty } = useUsers();
+  const [query, setQuery] = useState("");
 
   return (
     <div className="flex-1 flex flex-col border-r">
-      <div className="border-b p-4">
+      <div className="p-4">
         <h1 className="text-base font-medium text-foreground">
           People you can message
         </h1>
       </div>
+      <SearchField
+        value={query}
+        onChange={setQuery}
+        className="px-4 pb-6 pt-1"
+      />
       <div className="flex-1 w-full overflow-y-auto">
         {isLoading && <LoadingPlaceholder />}
 
