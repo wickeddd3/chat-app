f51433e6c0abff67dca81d9fdc92add4d79aabf7
wickeddd3 <pablophilipandrew@gmail.com>
2026-05-03 14:05:43 +0800

    feat: add debounce util

---
+++ chat-app-frontend/src/shared/utils/debounce.ts
@@ -0,0 +1,14 @@
+export function debounce<T extends (...args: any[]) => void>(
+  func: T,
+  delay: number,
+): (...args: Parameters<T>) => void {
+  let timeoutId: ReturnType<typeof setTimeout>;
+
+  return (...args: Parameters<T>) => {
+    clearTimeout(timeoutId);
+
+    timeoutId = setTimeout(() => {
+      func(...args);
+    }, delay);
+  };
+}
