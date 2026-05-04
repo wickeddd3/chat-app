d08a570ce2ab2958067b53d686cf33e12ebd2f54
wickeddd3 <pablophilipandrew@gmail.com>
2026-05-05 03:53:55 +0800

    feat: emit mark_as_read event on chart room mount

--- chat-app-frontend/src/features/message/chat-room/model/useChatRoom.ts
+++ chat-app-frontend/src/features/message/chat-room/model/useChatRoom.ts
@@ -34,6 +34,8 @@ export function useChatRoom(channelId: string, messages: Message[]) {
 
     // Join room on mount
     webSocketClient.emit("join_channel", channelId);
+    // Mark messages as read on mount
+    webSocketClient.emit("mark_as_read", { channelId });
     // Listen for incoming messages
     webSocketClient.on("receive_message", handleIncomingMessage);
 
