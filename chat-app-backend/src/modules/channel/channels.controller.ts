be244377a7abbb6a79675c2b65e772c16a36d23a
wickeddd3 <pablophilipandrew@gmail.com>
2026-05-04 15:37:35 +0800

    feat: fetch channel details by channelId and transform channel data

--- chat-app-backend/src/modules/channel/channels.controller.ts
+++ chat-app-backend/src/modules/channel/channels.controller.ts
@@ -3,7 +3,7 @@ import { type NextFunction, type Response, Router } from "express";
 import { ChannelsService } from "./channels.service";
 import { authMiddleware } from "@/middlewares/auth.middleware";
 import HttpException from "@/utils/http.exception";
-import { channelToInboxChannel } from "./channels.transformer";
+import { channelToChannelDetails, channelToInboxChannel } from "./channels.transformer";
 
 export class ChannelsController implements Controller {
   public path = "/channels";
@@ -15,17 +15,18 @@ export class ChannelsController implements Controller {
   }
 
   private initializeRoutes(): void {
-    this.router.get(`${this.path}/:targetUserId`, [authMiddleware], this.getChannel);
+    this.router.get(`${this.path}/:channelId`, [authMiddleware], this.getChannel);
     this.router.get(`${this.path}`, [authMiddleware], this.getChannels);
   }
 
   private getChannel = async (req: ControllerRequest, res: Response, next: NextFunction): Promise<void> => {
     try {
       const authUserId = req.user?.id || "";
-      const targetUserId = req.params.targetUserId as string;
-      const channel = await this.channelsService.getChannel(authUserId, targetUserId);
+      const channelId = req.params.channelId as string;
+      const channel = await this.channelsService.getChannel(authUserId, parseInt(channelId));
 
-      res.status(200).json(channel);
+      const transformedChannel = channel ? channelToChannelDetails(channel, authUserId) : null;
+      res.status(200).json(transformedChannel);
     } catch (error: any) {
       next(new HttpException(500, error?.message || "Failed to retrieve channels"));
     }
