15a3b7ac8e58da459ce16ef59ee967765a90246b
wickeddd3 <pablophilipandrew@gmail.com>
2026-05-06 18:21:09 +0800

    feat: add ChannelHeader

--- chat-app-frontend/src/entities/channel/index.ts
+++ chat-app-frontend/src/entities/channel/index.ts
@@ -1,2 +1,5 @@
+export { ChannelHeader } from "./ui/ChannelHeader";
+
 export type { Channel, InboxChannel } from "./model/channel.types";
+
 export { useChannel } from "./model/useChannel";
---
+++ chat-app-frontend/src/entities/channel/ui/ChannelHeader.tsx
@@ -0,0 +1,29 @@
+import type { ReactNode } from "react";
+import type { InboxChannel } from "../model/channel.types";
+import { ProfileAvatar } from "@/shared/ui/ProfileAvatar";
+
+export function ChannelHeader({
+  channel,
+  isOnline = false,
+  optionSlot,
+}: {
+  channel: InboxChannel | null;
+  isOnline?: boolean;
+  optionSlot?: ReactNode;
+}) {
+  if (!channel) return;
+
+  return (
+    <div className="w-full flex justify-between items-center py-3.5 px-4">
+      <div className="flex items-center gap-4">
+        <ProfileAvatar
+          imageSrc={channel.displayImage || ""}
+          isOnline={isOnline}
+          badge={true}
+        />
+        <h1 className="text-md font-medium">{channel.displayName}</h1>
+      </div>
+      {optionSlot}
+    </div>
+  );
+}
