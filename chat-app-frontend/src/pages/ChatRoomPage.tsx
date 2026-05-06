2196b19231a2802dc5b50d9840443f2ec279d795
wickeddd3 <pablophilipandrew@gmail.com>
2026-05-06 21:13:18 +0800

    feat: channel header online identification

--- chat-app-frontend/src/pages/ChatRoomPage.tsx
+++ chat-app-frontend/src/pages/ChatRoomPage.tsx
@@ -1,15 +1,39 @@
 import { useParams } from "react-router";
+import { useMemo } from "react";
 import { ChannelHeader, useChannel } from "@/entities/channel";
+import { useAuth } from "@/entities/auth";
 import { ChatRoom } from "@/features/message/chat-room";
-import { InfoIcon } from "lucide-react";
+import { ChannelDetails } from "@/features/channel/channel-details";
+import { usePresence } from "@/app/store/PresenceContext";
 
 export default function ChatRoomPage() {
   const { channelId } = useParams();
   const { channel } = useChannel(channelId || "");
+  const { onlineUsers, isOnline } = usePresence();
+  const { authId } = useAuth();
+
+  const online = useMemo(() => {
+    if (channel?.type === "GROUP") {
+      return channel.channelMembers.some(
+        (member) => member.user.id !== authId && isOnline(member.user.id),
+      );
+    }
+    if (channel?.type === "DIRECT") {
+      const otherUser = channel.channelMembers.find(
+        (member) => member.user.id !== authId && isOnline(member.user.id),
+      );
+      return !!otherUser;
+    }
+    return false;
+  }, [channel, authId, onlineUsers]);
 
   return (
     <div className="flex-1 flex flex-col">
-      <ChannelHeader channel={channel} optionSlot={<InfoIcon />} />
+      <ChannelHeader
+        channel={channel}
+        isOnline={online}
+        optionSlot={<ChannelDetails channel={channel} />}
+      />
       <ChatRoom channelId={channelId || ""} />
     </div>
   );
