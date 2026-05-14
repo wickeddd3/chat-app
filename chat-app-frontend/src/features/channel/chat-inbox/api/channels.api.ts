e12d93cd4bc326dacb57fbd01b6f89569d47f37d
wickeddd3 <pablophilipandrew@gmail.com>
2026-05-14 14:14:30 +0800

    feat: add cursor query param on api request

--- chat-app-frontend/src/features/channel/chat-inbox/api/channels.api.ts
+++ chat-app-frontend/src/features/channel/chat-inbox/api/channels.api.ts
@@ -1,10 +1,11 @@
-import type { InboxChannel } from "@/entities/channel";
+import type { PaginatedInboxChannel } from "@/entities/channel";
 import apiRequest from "@/shared/lib/axios.client";
 
-export async function getInbox(): Promise<InboxChannel[]> {
+export async function getInbox<T>(cursor: T): Promise<PaginatedInboxChannel> {
   try {
+    const queryParams = cursor ? `?cursor=${cursor}` : "";
     const { data } = await apiRequest({
-      url: "api/channels",
+      url: `api/channels${queryParams}`,
     }).get();
     return data;
   } catch (error) {
