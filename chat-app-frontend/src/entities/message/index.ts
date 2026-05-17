70e781fd8d05a44c7526edf9303c4c26cfb821f3
wickeddd3 <pablophilipandrew@gmail.com>
2026-05-17 21:16:49 +0800

    feat: export PaginatedMessage type

--- chat-app-frontend/src/entities/message/index.ts
+++ chat-app-frontend/src/entities/message/index.ts
@@ -1,2 +1,6 @@
 export { MessageBubble } from "./ui/MessageBubble";
-export type { Message, InboxItem } from "./model/message.types";
+export type {
+  PaginatedMessage,
+  Message,
+  InboxItem,
+} from "./model/message.types";
