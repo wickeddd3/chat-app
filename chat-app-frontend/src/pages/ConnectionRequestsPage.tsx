dfd14bd1dfe2766e9fad9154433554cb70c87cb2
wickeddd3 <pablophilipandrew@gmail.com>
2026-05-26 02:32:07 +0800

    feat: show received and sent connection request list

--- chat-app-frontend/src/pages/ConnectionRequestsPage.tsx
+++ chat-app-frontend/src/pages/ConnectionRequestsPage.tsx
@@ -1,10 +1,51 @@
+import {
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger,
+} from "@/shared/ui/shadcn/tabs";
+import { SentConnectionRequests } from "@/features/connection/sent-connection";
+import { ReceivedConnectionRequests } from "@/features/connection/received-connection";
+import { AcceptConnectionButton } from "@/features/connection/accept-connection";
+
 export default function ConnectionRequestsPage() {
   return (
-    <div className="w-full h-full flex flex-col justify-center items-center">
-      <h1 className="text-3xl font-bold text-gray-400">
-        Connection Requests Page
-      </h1>
-      <h6 className="text-lg font-normal text-gray-400">Coming Soon</h6>
+    <div className="flex-1 flex flex-col max-h-full border-r">
+      <div className="flex justify-between items-center p-4">
+        <h1 className="text-base font-medium text-foreground">
+          Connection Requests
+        </h1>
+      </div>
+      <div className="flex-1 flex flex-col overflow-hidden">
+        <Tabs defaultValue="received" className="flex-1 flex flex-col min-h-0">
+          <TabsList className="w-fit px-4 bg-transparent shrink-0">
+            <TabsTrigger
+              value="received"
+              className="px-8 cursor-pointer rounded-full"
+            >
+              Received
+            </TabsTrigger>
+            <TabsTrigger
+              value="sent"
+              className="px-8 cursor-pointer rounded-full"
+            >
+              Sent
+            </TabsTrigger>
+          </TabsList>
+          <TabsContent
+            value="received"
+            className="flex-1 min-h-0 data-[state=active]:flex flex-col m-0"
+          >
+            <ReceivedConnectionRequests acceptButton={AcceptConnectionButton} />
+          </TabsContent>
+          <TabsContent
+            value="sent"
+            className="flex-1 min-h-0 data-[state=active]:flex flex-col m-0"
+          >
+            <SentConnectionRequests />
+          </TabsContent>
+        </Tabs>
+      </div>
     </div>
   );
 }
