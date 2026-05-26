e3a5cbe4e6ef2dd2f0f82544035fac40463635bc
wickeddd3 <pablophilipandrew@gmail.com>
2026-05-27 02:27:06 +0800

    feat: show username

--- chat-app-frontend/src/entities/connection/ui/ConnectionItem.tsx
+++ chat-app-frontend/src/entities/connection/ui/ConnectionItem.tsx
@@ -2,19 +2,20 @@ import { ProfileAvatar } from "@/shared/ui/ProfileAvatar";
 import type { ReactNode } from "react";
 
 export function ConnectionItem({
-  user: { name, image },
+  user: { name, image, username },
   optionSlot,
 }: {
-  user: { name: string; image?: string | null };
+  user: { name: string; username: string; image?: string | null };
   optionSlot?: ReactNode;
 }) {
   return (
-    <div className="flex items-center gap-4 border-b p-4 text-sm leading-tight whitespace-nowrap last:border-b-0 hover:bg-sidebar-accent hover:text-sidebar-accent-foreground">
+    <article className="flex items-center gap-4 border-b px-4 py-3 text-sm leading-tight whitespace-nowrap last:border-b-0 hover:bg-sidebar-accent hover:text-sidebar-accent-foreground">
       <ProfileAvatar imageSrc={image || ""} />
       <div className="flex-1 flex flex-col items-start gap-2">
         <span className="text-sm font-medium">{name}</span>
+        <span className="text-xs">{`@${username}`}</span>
       </div>
       {optionSlot}
-    </div>
+    </article>
   );
 }
