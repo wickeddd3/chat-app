7e6355cc3c2da035136b5f17fd89d3840ac3af3c
wickeddd3 <pablophilipandrew@gmail.com>
2026-05-27 14:36:11 +0800

    feat: add isLessThanADayOld date util

--- chat-app-frontend/src/shared/utils/date-format.ts
+++ chat-app-frontend/src/shared/utils/date-format.ts
@@ -1,4 +1,4 @@
-import { formatDistanceToNow, isValid } from "date-fns";
+import { differenceInHours, formatDistanceToNow, isValid } from "date-fns";
 
 export function dateToString(date: string) {
   return new Date(date).toLocaleTimeString([], {
@@ -16,4 +16,16 @@ export function dateToNow(date: string): string {
     includeSeconds: true,
     addSuffix: true,
   });
+}
+
+export function isLessThanADayOld(date: string): boolean {
+  if (!isValid(new Date(date))) {
+    return false;
+  }
+
+  const dateToCheck = new Date(date);
+  const dateNow = new Date();
+  const isLessThanADayOld = differenceInHours(dateNow, dateToCheck) < 24;
+
+  return isLessThanADayOld;
 }
