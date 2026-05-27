4892d6da4e93bce3f7a42680a841d9a1b54284a8
wickeddd3 <pablophilipandrew@gmail.com>
2026-05-27 14:04:04 +0800

    feat: add dateToNow date formatter util

--- chat-app-frontend/src/shared/utils/date-format.ts
+++ chat-app-frontend/src/shared/utils/date-format.ts
@@ -1,6 +1,19 @@
+import { formatDistanceToNow, isValid } from "date-fns";
+
 export function dateToString(date: string) {
   return new Date(date).toLocaleTimeString([], {
     hour: "2-digit",
     minute: "2-digit",
+  });
+}
+
+export function dateToNow(date: string): string {
+  if (!isValid(new Date(date))) {
+    return "";
+  }
+
+  return formatDistanceToNow(new Date(date), {
+    includeSeconds: true,
+    addSuffix: true,
   });
 }
