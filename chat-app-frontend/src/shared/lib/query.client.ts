68d936c0f2daa2769d1b678b45a472672d39020d
wickeddd3 <pablophilipandrew@gmail.com>
2026-05-30 00:05:08 +0800

    feat: activate tanstack query devtools

--- chat-app-frontend/src/shared/lib/query.client.ts
+++ chat-app-frontend/src/shared/lib/query.client.ts
@@ -13,3 +13,13 @@ export const queryClient = new QueryClient({
     },
   },
 });
+
+// This code is only for TypeScript
+declare global {
+  interface Window {
+    __TANSTACK_QUERY_CLIENT__: import("@tanstack/query-core").QueryClient;
+  }
+}
+
+// This code is for all users
+window.__TANSTACK_QUERY_CLIENT__ = queryClient;
