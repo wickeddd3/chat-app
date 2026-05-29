1c1d1ac502c7f2facb8a9f08bbdc3d921ca86006
wickeddd3 <pablophilipandrew@gmail.com>
2026-05-29 23:59:09 +0800

    feat: send-connection optimistic update

--- chat-app-frontend/src/features/connection/send-connection/model/useSendConnection.ts
+++ chat-app-frontend/src/features/connection/send-connection/model/useSendConnection.ts
@@ -1,7 +1,8 @@
 import { useMutation, type UseMutateFunction } from "@tanstack/react-query";
 import { sendConnectionRequestApi } from "../api/connections.api";
-import type { Channel } from "@/entities/channel";
+import type { Connection } from "@/entities/connection";
 import { toast } from "sonner";
+import { REACT_QUERY_KEYS } from "@/shared/config/react-query-keys";
 
 interface SendConnectionRequestData {
   receiverId: string;
@@ -9,7 +10,7 @@ interface SendConnectionRequestData {
 
 export function useSendConnection(): {
   sendConnectionRequest: UseMutateFunction<
-    Channel,
+    Connection,
     Error,
     SendConnectionRequestData,
     unknown
@@ -17,13 +18,43 @@ export function useSendConnection(): {
   isPending: boolean;
   error: unknown;
 } {
+  const queryKey = REACT_QUERY_KEYS["SENT_CONNECTION_REQUESTS"];
+
   const { mutate, isPending, error } = useMutation({
     mutationFn: (formData: SendConnectionRequestData) =>
       sendConnectionRequestApi(formData),
-    onSuccess: () => {
+    onSuccess: (data, variables, onMutate, context) => {
       toast.success("Connection request sent", {
         position: "bottom-right",
       });
+
+      const newRequest: Connection = {
+        ...data,
+      };
+
+      // Update sent requests list to include the new request
+      context.client.setQueryData(
+        queryKey,
+        (old: { pages: { connections: Connection[] }[] }) => {
+          if (!old) return old;
+
+          return {
+            ...old,
+            pages: old.pages.map(
+              (page: { connections: Connection[] }, index) => {
+                // Prepend only to page index 0 (the initial loaded batch view)
+                if (index === 0) {
+                  return {
+                    ...page,
+                    connections: [newRequest, ...page.connections],
+                  };
+                }
+                return page;
+              },
+            ),
+          };
+        },
+      );
     },
     onError: (error) => {
       toast.error("Connection request failed", {
