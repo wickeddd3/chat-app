8707fe61244bdc9e46115a2ed38382bb78717b59
wickeddd3 <pablophilipandrew@gmail.com>
2026-05-29 20:54:36 +0800

    feat: accept-connection optimistic update

--- chat-app-frontend/src/features/connection/accept-connection/model/useAcceptConnection.ts
+++ chat-app-frontend/src/features/connection/accept-connection/model/useAcceptConnection.ts
@@ -2,23 +2,36 @@ import { useMutation, type UseMutateFunction } from "@tanstack/react-query";
 import { acceptConnectionRequestApi } from "../api/connections.api";
 import type { Channel } from "@/entities/channel";
 import { toast } from "sonner";
+import { optimisticUpdate } from "./optimistic-update";
+import { REACT_QUERY_KEYS } from "@/shared/config/react-query-keys";
 
 export function useAcceptConnection(): {
   acceptConnectionRequest: UseMutateFunction<Channel, Error, string, unknown>;
   isPending: boolean;
   error: unknown;
 } {
+  const queryKey = REACT_QUERY_KEYS["RECEIVED_CONNECTION_REQUEST"];
+
   const { mutate, isPending, error } = useMutation({
     mutationFn: (id: string) => acceptConnectionRequestApi(id),
-    onSuccess: () => {
-      toast.success("Connection request accepted", {
+    onMutate: (connectionId, context) =>
+      optimisticUpdate(connectionId, context),
+    onError: (err, connectionId, onMutateResult, context) => {
+      // Rollback to previous state on failure
+      if (onMutateResult?.previousRequests) {
+        context.client.setQueryData(queryKey, onMutateResult.previousRequests);
+      }
+      toast.error("Failed to accept request", {
+        description: "Error occurred while accepting connection request",
         position: "bottom-right",
       });
     },
-    onError: (error) => {
-      toast.error("Connection request failed", {
-        description:
-          error?.message || "Error occurred while accepting connection request",
+    onSettled: (data, error, variables, onMutateResult, context) => {
+      // Invalidate to synchronize completely with DB
+      context.client.invalidateQueries({ queryKey });
+    },
+    onSuccess: () => {
+      toast.success("Connection request accepted", {
         position: "bottom-right",
       });
     },
