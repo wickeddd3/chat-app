26ff8f10c71d92603eb9fcce9bf083bad61dbac9
wickeddd3 <pablophilipandrew@gmail.com>
2026-06-02 17:53:16 +0800

    feat: use QueryParams type

--- chat-app-frontend/src/features/message/chat-room/api/messages.api.ts
+++ chat-app-frontend/src/features/message/chat-room/api/messages.api.ts
@@ -1,14 +1,14 @@
 import type { Message, PaginatedMessage } from "@/entities/message";
 import apiRequest from "@/shared/lib/axios.client";
 import type { ApiResponse } from "@/shared/types/api-response.type";
-import { toQueryParams } from "@/shared/utils/query-params";
+import { toQueryParams, type QueryParams } from "@/shared/utils/query-params";
 
 export async function getMessagesApi({
   channelId,
   params,
 }: {
   channelId: string;
-  params: Record<string, any>;
+  params: QueryParams;
 }): Promise<PaginatedMessage> {
   const queryParams = toQueryParams(params);
   const url = `/api/messages/${channelId}${queryParams}`;
