038c9d555435368cd99700223ce1ce18d4fa45e8
wickeddd3 <pablophilipandrew@gmail.com>
2026-06-11 22:01:59 +0800

    feat: add AuthContext

--- chat-app-frontend/src/app/index.tsx
+++ chat-app-frontend/src/app/index.tsx
@@ -4,15 +4,18 @@ import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "@/shared/lib/query.client";
 import { TooltipProvider } from "@/shared/ui/shadcn/tooltip";
 import { PresenceProvider } from "@/app/store/PresenceContext";
+import { AuthProvider } from "@/app/store/AuthContext";
 
 export function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <TooltipProvider>
-        <PresenceProvider>
-          <RouterProvider router={router} />
-        </PresenceProvider>
-      </TooltipProvider>
+      <AuthProvider>
+        <TooltipProvider>
+          <PresenceProvider>
+            <RouterProvider router={router} />
+          </PresenceProvider>
+        </TooltipProvider>
+      </AuthProvider>
     </QueryClientProvider>
   );
 }
