c37b1fff3a1bc820a6054679252afa095847f61c
wickeddd3 <pablophilipandrew@gmail.com>
2026-06-13 04:15:40 +0800

    cut: remove email when saving profile

--- chat-app-backend/prisma/seed/users.seeder.ts
+++ chat-app-backend/prisma/seed/users.seeder.ts
@@ -25,28 +25,26 @@ async function createAuthUser(email: string, password: string) {
 }
 
 // Create the profile in the database
-async function createProfile(userId: string, email: string, username: string, name: string) {
+async function createProfile(userId: string, username: string, name: string) {
   try {
     const profile = await prisma.user.upsert({
       where: { id: userId },
       update: {
         username: username,
-        email: email,
       },
       create: {
         id: userId,
-        email: email,
         username: username,
         name: name,
       },
     });
 
     if (!profile) {
-      console.error(`❌ Failed to upsert profile for ${email}`);
+      console.error(`❌ Failed to upsert profile for ${username}`);
       return null;
     }
 
-    console.log(`📝 Profile upserted for ${username} (${email})`);
+    console.log(`📝 Profile upserted for ${name} (${username})`);
     return profile;
   } catch (err) {
     console.error(`❌ Prisma Error for ${userId}:`, err);
@@ -76,7 +74,7 @@ export async function usersSeeder() {
     }
 
     // 2. Create Profile in Database
-    await createProfile(userId, email, username, name);
+    await createProfile(userId, username, name);
 
     console.log(`📝 User created for ${username} (${email})`);
   }
