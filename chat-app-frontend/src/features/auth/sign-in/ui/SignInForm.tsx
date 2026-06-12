df66aab7f27f72849fae25fb164aec0e290549ee
wickeddd3 <pablophilipandrew@gmail.com>
2026-06-12 18:35:57 +0800

    cut: remove card field description

--- chat-app-frontend/src/features/auth/sign-in/ui/SignInForm.tsx
+++ chat-app-frontend/src/features/auth/sign-in/ui/SignInForm.tsx
@@ -39,63 +39,57 @@ export function SignInForm({
   }
 
   return (
-    <div className={cn("flex flex-col gap-6", className)} {...props}>
-      <Card>
-        <CardHeader className="text-center">
-          <CardTitle className="text-xl">Welcome back</CardTitle>
-          <CardDescription>
-            Login with your Apple or Google account
-          </CardDescription>
-        </CardHeader>
-        <CardContent>
-          <form id="sign-in-form" onSubmit={form.handleSubmit(onSubmit)}>
-            <FieldGroup className="gap-4">
-              <Field>
-                <Button variant="outline" type="button">
-                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                    <path
-                      d="M12.48 10.92v3.28h7.84c-.24 1.84-.853 3.187-1.787 4.133-1.147 1.147-2.933 2.4-6.053 2.4-4.827 0-8.6-3.893-8.6-8.72s3.773-8.72 8.6-8.72c2.6 0 4.507 1.027 5.907 2.347l2.307-2.307C18.747 1.44 16.133 0 12.48 0 5.867 0 .307 5.387.307 12s5.56 12 12.173 12c3.573 0 6.267-1.173 8.373-3.36 2.16-2.16 2.84-5.213 2.84-7.667 0-.76-.053-1.467-.173-2.053H12.48z"
-                      fill="currentColor"
-                    />
-                  </svg>
-                  Login with Google
-                </Button>
-              </Field>
-              <FieldSeparator className="*:data-[slot=field-separator-content]:bg-card">
-                Or continue with
-              </FieldSeparator>
-              <TextField
-                control={form.control}
-                name="email"
-                id="email"
-                label="Email"
-              />
-              <TextField
-                control={form.control}
-                name="password"
-                id="password"
-                label="Password"
-              />
-              <Field>
-                <Button type="submit" className="cursor-pointer">
-                  Login
-                </Button>
-                <FieldDescription className="text-center">
-                  <Link to="/auth/sign-up">Forgot your password?</Link>
-                </FieldDescription>
-                <FieldDescription className="text-center">
-                  Don&apos;t have an account?{" "}
-                  <Link to="/auth/sign-up">Sign up</Link>
-                </FieldDescription>
-              </Field>
-            </FieldGroup>
-          </form>
-        </CardContent>
-      </Card>
-      <FieldDescription className="px-6 text-center">
-        By clicking continue, you agree to our <a href="#">Terms of Service</a>{" "}
-        and <a href="#">Privacy Policy</a>.
-      </FieldDescription>
-    </div>
+    <Card className={cn("flex flex-col gap-6", className)} {...props}>
+      <CardHeader className="text-center">
+        <CardTitle className="text-xl">Welcome back</CardTitle>
+        <CardDescription>
+          Login with your Apple or Google account
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <form id="sign-in-form" onSubmit={form.handleSubmit(onSubmit)}>
+          <FieldGroup className="gap-4">
+            <Field>
+              <Button variant="outline" type="button">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                  <path
+                    d="M12.48 10.92v3.28h7.84c-.24 1.84-.853 3.187-1.787 4.133-1.147 1.147-2.933 2.4-6.053 2.4-4.827 0-8.6-3.893-8.6-8.72s3.773-8.72 8.6-8.72c2.6 0 4.507 1.027 5.907 2.347l2.307-2.307C18.747 1.44 16.133 0 12.48 0 5.867 0 .307 5.387.307 12s5.56 12 12.173 12c3.573 0 6.267-1.173 8.373-3.36 2.16-2.16 2.84-5.213 2.84-7.667 0-.76-.053-1.467-.173-2.053H12.48z"
+                    fill="currentColor"
+                  />
+                </svg>
+                Login with Google
+              </Button>
+            </Field>
+            <FieldSeparator className="*:data-[slot=field-separator-content]:bg-card">
+              Or continue with
+            </FieldSeparator>
+            <TextField
+              control={form.control}
+              name="email"
+              id="email"
+              label="Email"
+            />
+            <TextField
+              control={form.control}
+              name="password"
+              id="password"
+              label="Password"
+            />
+            <Field>
+              <Button type="submit" className="cursor-pointer">
+                Login
+              </Button>
+              <FieldDescription className="text-center">
+                <Link to="/auth/sign-up">Forgot your password?</Link>
+              </FieldDescription>
+              <FieldDescription className="text-center">
+                Don&apos;t have an account?{" "}
+                <Link to="/auth/sign-up">Sign up</Link>
+              </FieldDescription>
+            </Field>
+          </FieldGroup>
+        </form>
+      </CardContent>
+    </Card>
   );
 }
--- chat-app-frontend/src/features/auth/sign-up/ui/SignUpForm.tsx
+++ chat-app-frontend/src/features/auth/sign-up/ui/SignUpForm.tsx
@@ -42,80 +42,73 @@ export function SignUpForm({
   }
 
   return (
-    <div className={cn("flex flex-col gap-6", className)} {...props}>
-      <Card>
-        <CardHeader className="text-center">
-          <CardTitle className="text-xl">Create your account</CardTitle>
-          <CardDescription>
-            Enter your email below to create your account
-          </CardDescription>
-        </CardHeader>
-        <CardContent>
-          <form id="sign-up-form" onSubmit={form.handleSubmit(onSubmit)}>
-            <FieldGroup>
-              <Field>
-                <Button variant="outline" type="button">
-                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                    <path
-                      d="M12.48 10.92v3.28h7.84c-.24 1.84-.853 3.187-1.787 4.133-1.147 1.147-2.933 2.4-6.053 2.4-4.827 0-8.6-3.893-8.6-8.72s3.773-8.72 8.6-8.72c2.6 0 4.507 1.027 5.907 2.347l2.307-2.307C18.747 1.44 16.133 0 12.48 0 5.867 0 .307 5.387.307 12s5.56 12 12.173 12c3.573 0 6.267-1.173 8.373-3.36 2.16-2.16 2.84-5.213 2.84-7.667 0-.76-.053-1.467-.173-2.053H12.48z"
-                      fill="currentColor"
-                    />
-                  </svg>
-                  Sign up with Google
-                </Button>
-              </Field>
-              <FieldSeparator className="*:data-[slot=field-separator-content]:bg-card">
-                Or continue with
-              </FieldSeparator>
-              <TextField
-                control={form.control}
-                name="name"
-                id="name"
-                label="Full Name"
-              />
-              <TextField
-                control={form.control}
-                name="email"
-                id="email"
-                label="Email"
-              />
-              <TextField
-                control={form.control}
-                name="username"
-                id="username"
-                label="Username"
-              />
-              <Field>
-                <Field className="grid grid-cols-2 gap-4">
-                  <TextField
-                    control={form.control}
-                    name="password"
-                    id="password"
-                    label="Password"
+    <Card className={cn("flex flex-col gap-6", className)} {...props}>
+      <CardHeader className="text-center">
+        <CardTitle className="text-xl">Create your account</CardTitle>
+        <CardDescription>
+          Enter your email below to create your account
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <form id="sign-up-form" onSubmit={form.handleSubmit(onSubmit)}>
+          <FieldGroup>
+            <Field>
+              <Button variant="outline" type="button">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                  <path
+                    d="M12.48 10.92v3.28h7.84c-.24 1.84-.853 3.187-1.787 4.133-1.147 1.147-2.933 2.4-6.053 2.4-4.827 0-8.6-3.893-8.6-8.72s3.773-8.72 8.6-8.72c2.6 0 4.507 1.027 5.907 2.347l2.307-2.307C18.747 1.44 16.133 0 12.48 0 5.867 0 .307 5.387.307 12s5.56 12 12.173 12c3.573 0 6.267-1.173 8.373-3.36 2.16-2.16 2.84-5.213 2.84-7.667 0-.76-.053-1.467-.173-2.053H12.48z"
+                    fill="currentColor"
                   />
-                  <TextField
-                    control={form.control}
-                    name="confirmPassword"
-                    id="confirmPassword"
-                    label="Confirm Password"
-                  />
-                </Field>
+                </svg>
+                Sign up with Google
+              </Button>
+            </Field>
+            <FieldSeparator className="*:data-[slot=field-separator-content]:bg-card">
+              Or continue with
+            </FieldSeparator>
+            <TextField
+              control={form.control}
+              name="name"
+              id="name"
+              label="Full Name"
+            />
+            <TextField
+              control={form.control}
+              name="email"
+              id="email"
+              label="Email"
+            />
+            <TextField
+              control={form.control}
+              name="username"
+              id="username"
+              label="Username"
+            />
+            <Field>
+              <Field className="grid grid-cols-2 gap-4">
+                <TextField
+                  control={form.control}
+                  name="password"
+                  id="password"
+                  label="Password"
+                />
+                <TextField
+                  control={form.control}
+                  name="confirmPassword"
+                  id="confirmPassword"
+                  label="Confirm Password"
+                />
               </Field>
-              <Field>
-                <Button type="submit">Create Account</Button>
-                <FieldDescription className="text-center">
-                  Already have an account?{" "}
-                  <Link to="/auth/sign-in">Sign in</Link>
-                </FieldDescription>
-              </Field>
-            </FieldGroup>
-          </form>
-        </CardContent>
-      </Card>
-      <FieldDescription className="px-6 text-center">
-        By clicking continue, you agree to our <a href="#">Terms of Service</a>{" "}
-        and <a href="#">Privacy Policy</a>.
-      </FieldDescription>
-    </div>
+            </Field>
+            <Field>
+              <Button type="submit">Create Account</Button>
+              <FieldDescription className="text-center">
+                Already have an account? <Link to="/auth/sign-in">Sign in</Link>
+              </FieldDescription>
+            </Field>
+          </FieldGroup>
+        </form>
+      </CardContent>
+    </Card>
   );
 }
