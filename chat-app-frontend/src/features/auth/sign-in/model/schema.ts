fe971684039d13d3d03d9e1b3c008921a5c85992
wickeddd3 <pablophilipandrew@gmail.com>
2026-06-12 16:31:13 +0800

    feat: signin and signup email field required validation

--- chat-app-frontend/src/features/auth/sign-in/model/schema.ts
+++ chat-app-frontend/src/features/auth/sign-in/model/schema.ts
@@ -1,7 +1,11 @@
 import { z } from "zod";
 
 export const SignInFormSchema = z.object({
-  email: z.email({ pattern: z.regexes.email }),
+  email: z
+    .string()
+    .trim()
+    .min(1, "Email is required")
+    .email({ pattern: z.regexes.email }),
   password: z.string().trim().min(1, "Password required").max(100),
 });
 
--- chat-app-frontend/src/features/auth/sign-up/model/schema.ts
+++ chat-app-frontend/src/features/auth/sign-up/model/schema.ts
@@ -3,7 +3,11 @@ import { z } from "zod";
 export const SignUpFormSchema = z
   .object({
     name: z.string().trim().min(1, "Name is required").max(100),
-    email: z.email({ pattern: z.regexes.email }),
+    email: z
+      .string()
+      .trim()
+      .min(1, "Email is required")
+      .email({ pattern: z.regexes.email }),
     username: z.string().trim().min(5, "Username is required").max(100),
     password: z
       .string()
