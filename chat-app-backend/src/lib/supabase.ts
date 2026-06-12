aaa842ac0963dd61453f40342825cdfeb9d0e2fa
wickeddd3 <pablophilipandrew@gmail.com>
2026-06-13 03:17:07 +0800

    feat: use service role key

--- chat-app-backend/src/lib/supabase.ts
+++ chat-app-backend/src/lib/supabase.ts
@@ -1,32 +1,16 @@
-import { SUPABASE_PUBLISHABLE_KEY, SUPABASE_URL } from "@/config/app.config";
+import { SUPABASE_SERVICE_ROLE_KEY, SUPABASE_URL } from "@/config/app.config";
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = SUPABASE_URL;
-const supabaseKey = SUPABASE_PUBLISHABLE_KEY;
+const supabaseKey = SUPABASE_SERVICE_ROLE_KEY;
 
 if (!supabaseUrl || !supabaseKey) {
-  console.error("❌ Supabase Env Missing:", { supabaseUrl, supabaseKey });
-  throw new Error("SUPABASE_URL and SUPABASE_PUBLISHABLE_KEY are required.");
+  throw new Error("SUPABASE_URL and SUPABASE_SERVICE_ROLE_KEY are required.");
 }
 
-export const supabase = createClient(supabaseUrl, supabaseKey);
-
-export const verifySupabaseToken = async (token: string) => {
-  if (!token) return null;
-
-  // Remove "Bearer " prefix if it exists
-  const jwt = token.replace("Bearer ", "");
-
-  // This calls Supabase to verify the token is valid and active
-  const {
-    data: { user },
-    error,
-  } = await supabase.auth.getUser(jwt);
-
-  if (error || !user) {
-    console.error("Auth Error:", error?.message);
-    return null;
-  }
-
-  return user.id;
-};
+export const supabase = createClient(supabaseUrl, supabaseKey, {
+  auth: {
+    persistSession: false, // Crucial for stateless Express backends
+    autoRefreshToken: false,
+  },
+});
