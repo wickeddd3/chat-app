50a4b7c9eb3ed15813e4b2fbbf955dc37233f127
wickeddd3 <pablophilipandrew@gmail.com>
2026-06-13 21:51:13 +0800

    feat: add router type and bind routers to container

--- chat-app-backend/src/config/inversify.config.ts
+++ chat-app-backend/src/config/inversify.config.ts
@@ -7,18 +7,22 @@ import { PrismaClient } from "@/prisma/client";
 import { redisClient } from "@/lib/redis";
 import { eventDispatcher } from "@/lib/event-dispatcher";
 
+import { AuthRouter } from "@/modules/auth/auth.router";
 import { AuthController } from "@/modules/auth/auth.controller";
 import { AuthService } from "@/modules/auth/auth.service";
 import { AuthRepository } from "@/modules/auth/auth.repository";
 
+import { UsersRouter } from "@/modules/user/users.router";
 import { UsersController } from "@/modules/user/users.controller";
 import { UsersRepository } from "@/modules/user/users.repository";
 import { UsersService } from "@/modules/user/users.service";
 
+import { ChannelsRouter } from "@/modules/channel/channels.router";
 import { ChannelsController } from "@/modules/channel/channels.controller";
 import { ChannelsService } from "@/modules/channel/channels.service";
 import { ChannelsRepository } from "@/modules/channel/channels.repository";
 
+import { MessagesRouter } from "@/modules/message/messages.router";
 import { MessagesController } from "@/modules/message/messages.controller";
 import { MessagesService } from "@/modules/message/messages.service";
 import { MessagesRepository } from "@/modules/message/messages.repository";
@@ -26,10 +30,12 @@ import { MessagesRepository } from "@/modules/message/messages.repository";
 import { MessageReceiptsService } from "@/modules/message-receipt/message-receipts.service";
 import { MessageReceiptsRepository } from "@/modules/message-receipt/message-receipts.repository";
 
+import { ConnectionsRouter } from "@/modules/connection/connections.router";
 import { ConnectionsController } from "@/modules/connection/connections.controller";
 import { ConnectionsService } from "@/modules/connection/connections.service";
 import { ConnectionsRepository } from "@/modules/connection/connections.repository";
 
+import { NotificationsRouter } from "@/modules/notification/notifications.router";
 import { NotificationsRepository } from "@/modules/notification/notifications.repository";
 import { NotificationsService } from "@/modules/notification/notifications.service";
 import { NotificationsController } from "@/modules/notification/notifications.controller";
@@ -57,18 +63,22 @@ container.bind(TYPES.RedisClient).toConstantValue(redisClient);
 container.bind(TYPES.EventDispatcher).toConstantValue(eventDispatcher);
 
 // Bind domain layers
+container.bind<AuthRouter>(TYPES.AuthRouter).to(AuthRouter);
 container.bind<AuthController>(TYPES.AuthController).to(AuthController);
 container.bind<AuthService>(TYPES.AuthService).to(AuthService);
 container.bind<AuthRepository>(TYPES.AuthRepository).to(AuthRepository);
 
+container.bind<UsersRouter>(TYPES.UsersRouter).to(UsersRouter);
 container.bind<UsersController>(TYPES.UsersController).to(UsersController);
 container.bind<UsersService>(TYPES.UsersService).to(UsersService);
 container.bind<UsersRepository>(TYPES.UsersRepository).to(UsersRepository);
 
+container.bind<ChannelsRouter>(TYPES.ChannelsRouter).to(ChannelsRouter);
 container.bind<ChannelsController>(TYPES.ChannelsController).to(ChannelsController);
 container.bind<ChannelsService>(TYPES.ChannelsService).to(ChannelsService);
 container.bind<ChannelsRepository>(TYPES.ChannelsRepository).to(ChannelsRepository);
 
+container.bind<MessagesRouter>(TYPES.MessagesRouter).to(MessagesRouter);
 container.bind<MessagesController>(TYPES.MessagesController).to(MessagesController);
 container.bind<MessagesService>(TYPES.MessagesService).to(MessagesService);
 container.bind<MessagesRepository>(TYPES.MessagesRepository).to(MessagesRepository);
@@ -76,10 +86,12 @@ container.bind<MessagesRepository>(TYPES.MessagesRepository).to(MessagesReposito
 container.bind<MessageReceiptsService>(TYPES.MessageReceiptsService).to(MessageReceiptsService);
 container.bind<MessageReceiptsRepository>(TYPES.MessageReceiptsRepository).to(MessageReceiptsRepository);
 
+container.bind<ConnectionsRouter>(TYPES.ConnectionsRouter).to(ConnectionsRouter);
 container.bind<ConnectionsController>(TYPES.ConnectionsController).to(ConnectionsController);
 container.bind<ConnectionsService>(TYPES.ConnectionsService).to(ConnectionsService);
 container.bind<ConnectionsRepository>(TYPES.ConnectionsRepository).to(ConnectionsRepository);
 
+container.bind<NotificationsRouter>(TYPES.NotificationsRouter).to(NotificationsRouter);
 container.bind<NotificationsController>(TYPES.NotificationsController).to(NotificationsController);
 container.bind<NotificationsService>(TYPES.NotificationsService).to(NotificationsService);
 container.bind<NotificationsRepository>(TYPES.NotificationsRepository).to(NotificationsRepository);
