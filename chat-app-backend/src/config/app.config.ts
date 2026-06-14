1f9bb107914912fc1e556c6730e3aab916bc740f
wickeddd3 <pablophilipandrew@gmail.com>
2026-06-14 18:53:30 +0800

    feat: add API_URL

--- chat-app-backend/src/config/app.config.ts
+++ chat-app-backend/src/config/app.config.ts
@@ -1,5 +1,7 @@
 export const APP_URL = process.env.APP_URL ?? "";
 
+export const API_URL = process.env.API_URL ?? "";
+
 export const PORT = process.env.PORT ? parseInt(process.env.PORT, 10) : 4000;
 
 export const DATABASE_URL = process.env.DATABASE_URL;
