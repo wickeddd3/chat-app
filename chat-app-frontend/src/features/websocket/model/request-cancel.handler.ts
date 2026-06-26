d139a8fb4508fcc4e6ff86bc41959e96c3aebada
wickeddd3 <pablophilipandrew@gmail.com>
2026-06-27 02:02:39 +0800

    feat: remove notification and decrement unread count stats on cancel request

--- chat-app-frontend/src/features/websocket/model/request-cancel.handler.ts
+++ chat-app-frontend/src/features/websocket/model/request-cancel.handler.ts
@@ -1,5 +1,6 @@
 import type { QueryClient } from "@tanstack/react-query";
 import type { Connection } from "@/entities/connection";
+import type { Notification } from "@/entities/notification";
 import { REACT_QUERY_KEYS } from "@/shared/config/react-query-keys";
 
 // Remove connection request to received request cache
@@ -7,6 +8,7 @@ export const handleCancelRequest = (
   queryClient: QueryClient,
   connectionId: string,
 ) => {
+  // Remove new connection request from existing received connection requests cache
   queryClient.setQueryData(
     REACT_QUERY_KEYS["RECEIVED_CONNECTION_REQUESTS"],
     (old: { pages: { connections: Connection[] }[] }) => {
@@ -22,6 +24,42 @@ export const handleCancelRequest = (
           ),
         })),
       };
+    },
+  );
+
+  // Remove new connection request notification from existing notifications cache
+  queryClient.setQueryData(
+    REACT_QUERY_KEYS["NOTIFICATIONS"],
+    (old: { pages: { notifications: Notification[] }[] }) => {
+      if (!old) return old;
+
+      return {
+        ...old,
+        pages: old.pages.map((page: { notifications: Notification[] }) => {
+          return {
+            ...page,
+            notifications: page.notifications.filter(
+              (notif: Notification) => notif.referenceId !== connectionId,
+            ),
+          };
+        }),
+      };
+    },
+  );
+
+  // Decrement pending request count and unread notifications count
+  queryClient.setQueryData(
+    REACT_QUERY_KEYS["UNREAD_COUNT_STATS"],
+    (old: Record<string, number>) => {
+      if (!old) return old;
+
+      const currentUnreadCountStats = { ...old };
+      currentUnreadCountStats["pendingRequestsCount"] =
+        currentUnreadCountStats["pendingRequestsCount"] - 1;
+      currentUnreadCountStats["unreadNotificationsCount"] =
+        currentUnreadCountStats["unreadNotificationsCount"] - 1;
+
+      return currentUnreadCountStats;
     },
   );
 };
