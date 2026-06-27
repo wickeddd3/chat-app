226d8566f90b6f713f9c42024777cae8512dded1
wickeddd3 <pablophilipandrew@gmail.com>
2026-06-27 14:54:10 +0800

    feat: update user connectionStatus on request decline

--- chat-app-frontend/src/features/websocket/model/request-declined.handler.ts
+++ chat-app-frontend/src/features/websocket/model/request-declined.handler.ts
@@ -1,11 +1,15 @@
 import type { QueryClient } from "@tanstack/react-query";
 import type { Connection } from "@/entities/connection";
+import type { User } from "@/entities/user";
 import { REACT_QUERY_KEYS } from "@/shared/config/react-query-keys";
 
 // Remove connection request to received request cache
 export const handleDeclinedRequest = (
   queryClient: QueryClient,
-  connectionId: string,
+  payload: {
+    receiverId: string;
+    connectionId: string;
+  },
 ) => {
   // Remove sent connection request from existing sent connection requests cache
   queryClient.setQueryData(
@@ -19,10 +23,31 @@ export const handleDeclinedRequest = (
         pages: old.pages.map((page: { connections: Connection[] }) => ({
           ...page,
           connections: page.connections.filter(
-            (req: Connection) => req.id !== connectionId,
+            (req: Connection) => req.id !== payload.connectionId,
           ),
         })),
       };
+    },
+  );
+
+  // Update users list to update user connectionStatus
+  queryClient.setQueryData(
+    [...REACT_QUERY_KEYS["USERS"], ""],
+    (old: User[]) => {
+      if (!old) return old;
+
+      const currentUsers = [...old];
+      const userIndex = currentUsers.findIndex(
+        (user) => user.id === payload.receiverId,
+      );
+
+      currentUsers[userIndex] = {
+        ...currentUsers[userIndex],
+        connectionId: null,
+        connectionStatus: "STRANGER",
+      };
+
+      return currentUsers;
     },
   );
 };
