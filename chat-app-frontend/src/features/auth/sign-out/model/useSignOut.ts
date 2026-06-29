375c623b3d4c26de58201505eed8155ac6b2f0f6
wickeddd3 <pablophilipandrew@gmail.com>
2026-06-29 16:43:04 +0800

    feat: clear tanstack query cache on logout

--- chat-app-frontend/src/features/auth/sign-out/model/useSignOut.ts
+++ chat-app-frontend/src/features/auth/sign-out/model/useSignOut.ts
@@ -1,11 +1,16 @@
 import { signOut } from "@/shared/lib/supabase-auth";
+import { useQueryClient } from "@tanstack/react-query";
 import { useNavigate } from "react-router";
 
 export function useSignOut() {
   const navigate = useNavigate();
+  const queryClient = useQueryClient();
 
   const logout = async () => {
     await signOut();
+
+    queryClient.clear(); // Wipe of the TanStack Query In-Memory Cache
+
     navigate("/");
   };
 
