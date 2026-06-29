2f1668a86dfd1621e6f0c6f6f22f648d9aeea868
wickeddd3 <pablophilipandrew@gmail.com>
2026-06-29 18:57:21 +0800

    feat: use query keys

--- chat-app-frontend/src/entities/channel/model/useChannel.ts
+++ chat-app-frontend/src/entities/channel/model/useChannel.ts
@@ -1,14 +1,20 @@
 import { useQuery } from "@tanstack/react-query";
 import { getChannel } from "../api/channels.api";
 import type { InboxChannel } from "./channel.types";
+import { createQueryKeys } from "@/shared/config/react-query-keys";
 
-export function useChannel(channelId: string): {
+export function useChannel(
+  channelId: string,
+  authId?: string,
+): {
   channel: InboxChannel | null;
   isLoading: boolean;
   error: unknown;
 } {
+  const keys = createQueryKeys(authId);
+
   const { data, isLoading, error } = useQuery({
-    queryKey: ["channel", channelId],
+    queryKey: keys.channel.details(channelId),
     queryFn: () => getChannel(channelId),
     enabled: !!channelId,
   });
