1df654179cb802943429d42e03c08e9b3a573f0c
wickeddd3 <pablophilipandrew@gmail.com>
2026-07-03 16:43:15 +0800

    cut: remove HomePage

--- chat-app-frontend/src/pages/HomePage.tsx
+++
@@ -1,8 +0,0 @@
-export default function HomePage() {
-  return (
-    <div className="w-full h-full flex flex-col justify-center items-center">
-      <h1 className="text-3xl font-bold text-muted-foreground">Home Page</h1>
-      <h6 className="text-lg font-normal text-muted-foreground">Coming Soon</h6>
-    </div>
-  );
-}
