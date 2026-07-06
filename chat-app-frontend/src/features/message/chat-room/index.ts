bfa2210f6f673fe45412e70af90d0e256114df2b
wickeddd3 <pablophilipandrew@gmail.com>
2026-07-07 02:35:32 +0800

    feat: add ChannelUnavailablePlaceholder

--- chat-app-frontend/src/features/message/chat-room/index.ts
+++ chat-app-frontend/src/features/message/chat-room/index.ts
@@ -1,2 +1,3 @@
 export { ChatRoom } from "./ui/ChatRoom";
 export { ContentPlaceholder } from "./ui/ContentPlaceholder";
+export { ChannelUnavailablePlaceholder } from "./ui/ChannelUnavailablePlaceholder";
